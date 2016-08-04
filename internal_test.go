c5dcb01377aba261421b0c15267949674f603b62
Ben Aldrich <ben.aldrich@logrhythm.com>
2016-08-04 11:33:52 -0600

    add tests

--- internal_test.go
+++ internal_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestDepthFirst(t *testing.T) {
 	defer log.Flush()
-	Convey("Add items with edges validate edges", t, func() {
+	Convey("Add items with edges validate edges, multiple layers of relations", t, func() {
 		//Don't add nodes at the same level for this test because
 		//they are stored in maps and will rearange your results randomly at the same level
 		g := NewDirectedGraph(1)
@@ -56,6 +56,48 @@ func TestDepthFirst(t *testing.T) {
 			So(OrderValid(Node("3"), Node("31"), r), ShouldBeTrue)
 			So(OrderValid(Node("3"), Node("32"), r), ShouldBeTrue)
 			So(OrderValid(Node("3"), Node("4"), r), ShouldBeTrue)
+		})
+
+		Convey("Validate 4 order", func() {
+			So(OrderValid(Node("1"), Node("4"), r), ShouldBeTrue)
+			So(OrderValid(Node("2"), Node("4"), r), ShouldBeTrue)
+			So(OrderValid(Node("3"), Node("4"), r), ShouldBeTrue)
+		})
+
+	})
+
+	Convey("Add items with edges validate edges, 5th layer", t, func() {
+		//Don't add nodes at the same level for this test because
+		//they are stored in maps and will rearange your results randomly at the same level
+		g := NewDirectedGraph(1)
+		g.SetEdge(Edge{F: Node("1"), T: Node("10"), W: 1})
+		g.SetEdge(Edge{F: Node("1"), T: Node("11"), W: 1})
+		g.SetEdge(Edge{F: Node("1"), T: Node("12"), W: 1})
+		g.SetEdge(Edge{F: Node("12"), T: Node("2"), W: 1})
+
+		g.SetEdge(Edge{F: Node("2"), T: Node("20"), W: 1})
+		g.SetEdge(Edge{F: Node("2"), T: Node("21"), W: 1})
+		g.SetEdge(Edge{F: Node("2"), T: Node("22"), W: 1})
+		g.SetEdge(Edge{F: Node("22"), T: Node("3"), W: 1})
+
+		g.SetEdge(Edge{F: Node("3"), T: Node("30"), W: 1})
+		g.SetEdge(Edge{F: Node("3"), T: Node("31"), W: 1})
+		g.SetEdge(Edge{F: Node("3"), T: Node("32"), W: 1})
+
+		g.SetEdge(Edge{F: Node("4"), T: Node("1"), W: 1})
+		g.SetEdge(Edge{F: Node("4"), T: Node("2"), W: 1})
+		g.SetEdge(Edge{F: Node("4"), T: Node("3"), W: 1})
+
+		g.SetEdge(Edge{F: Node("5"), T: Node("4"), W: 1})
+
+		r := DepthFirst(*g, Node("1"), nil)
+		So(len(r), ShouldEqual, 14)
+
+		Convey("Validate 5's order is after all of 4's deps", func() {
+			So(OrderValid(Node("1"), Node("5"), r), ShouldBeTrue)
+			So(OrderValid(Node("2"), Node("5"), r), ShouldBeTrue)
+			So(OrderValid(Node("3"), Node("5"), r), ShouldBeTrue)
+			So(OrderValid(Node("4"), Node("5"), r), ShouldBeTrue)
 		})
 
 	})
